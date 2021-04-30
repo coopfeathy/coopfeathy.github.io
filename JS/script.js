12f5eea0f1e502ab8ec5256eb167b7daaf6a0d1a
coopfeathy <cooperfeatherstone13@gmail.com>
2021-04-30 16:30:54 -0600

    changes

--- JS/script.js
+++ JS/script.js
@@ -2,4 +2,5 @@ let year = new Date().getFullYear();
      document.getElementById('year').innerHTML = year;
 
      var lastmod = document.lastModified;
-     document.getElementById('lastmod').innerHTML = lastmod;
+     // document.getElementById('lastmod').innerHTML = lastmod;
+     document.querySelector("#lastmod").innerHTML = lastmod;
