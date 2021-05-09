e6b6fb211e680cd641b91c6c9c5f1710a938aa59
coopfeathy <cooperfeatherstone13@gmail.com>
2021-05-08 22:43:36 -0600

    update

---
+++ lesson3/js/script.js
@@ -0,0 +1,6 @@
+let year = new Date().getFullYear();
+     document.getElementById('year').innerHTML = year;
+
+     var lastmod = document.lastModified;
+     // document.getElementById('lastmod').innerHTML = lastmod;
+     document.querySelector("#lastmod").innerHTML = lastmod;
