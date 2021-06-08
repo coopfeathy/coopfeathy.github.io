00002d7a7e2b13a027d4f6cfed457d6388af8cb5
coopfeathy <cooperfeatherstone13@gmail.com>
2021-06-07 20:58:03 -0600

    lazy load

---
+++ lesson7/lazyload/script.js
@@ -0,0 +1,29 @@
+const images = document.querySelectorAll("[data-src]");
+
+function preloadImage(img){
+    const src = img.getAttribute("data-src")
+    if(!src){
+        return;
+    }
+    img.src = src;
+    img.removeAttribute("data-src")
+}
+
+const imgoptions = {
+    threshold: 1
+};
+
+const imgObserver = new IntersectionObserver((entries, imgObserver) => {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting){
+            return;
+        } else {
+            preloadImage(entry.target);
+            imgObserver.unobserve(entry.target);
+        }
+    })
+}, imgoptions);
+
+images.forEach(image => {
+    imgObserver.observe(image);
+});
